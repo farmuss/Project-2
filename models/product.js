0e388f372b39870088308f98dc0e1168c93395ec
unknown <joanna.yange@gmail.com>
2018-09-19 15:30:41 -0600

    updated product.js and user.js files

--- models/product.js
+++ models/product.js
@@ -1,44 +1,15 @@
 
-
 module.exports = function(sequelize, DataTypes) {
   // Product is a variable and "product" is the table name, lowercase "sequelinze" is connection to table
   // defining as part of property
   var Product = sequelize.define("Product", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    price: { 
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    platform: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    }
+    
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    platform: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    description: DataTypes.TEXT
+
   });
   return Product;
 };
